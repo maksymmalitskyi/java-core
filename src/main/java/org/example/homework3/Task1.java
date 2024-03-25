c8f115c46ee84ca85392b5f6634f5faacbe286ab
maksymmalitskyi <maksym.malitskyi@gmail.com>
2024-03-25 11:09:16 +0200

    task1

---
+++ src/main/java/org/example/homework3/Task1.java
@@ -0,0 +1,20 @@
+package org.example.homework3;
+
+public class Task1 {
+
+    public static void main(String[] args) {
+        int a = 120;
+        int b = 60;
+
+        System.out.println(a + b);
+
+        System.out.println(a - b);
+
+        System.out.println(a * b);
+
+        System.out.println(a / b);
+
+
+
+    }
+}
